dfee7f00f1569403adcffe01119b4c58bc99567c
Serafima Nerush <2002ssn@gmail.com>
2021-10-13 16:17:36 -0700

    Initial commit

---
+++ SimpleCamera/PhotoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoViewController.swift
+//  SimpleCamera
+//
+//  Created by Simon Ng on 16/10/2019.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var image:UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.image = image
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Action methods
+    
+    @IBAction func save(sender: UIButton) {
+    }
+
+}
---
+++ SimpleCamera/SimpleCameraController.swift
@@ -0,0 +1,37 @@
+//
+//  SimpleCameraController.swift
+//  SimpleCamera
+//
+//  Created by Simon Ng on 16/10/2019.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class SimpleCameraController: UIViewController {
+
+    @IBOutlet var cameraButton:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Action methods
+    
+    @IBAction func capture(sender: UIButton) {
+    }
+
+    // MARK: - Segues
+    
+    @IBAction func unwindToCameraView(segue: UIStoryboardSegue) {
+    
+    }
+
+}
