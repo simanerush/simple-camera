8d917c15161e8f4fba48c1d5e8bde5253d5c6044
Sima Nerush <2002ssn@gmail.com>
2021-10-18 15:04:35 -0400

    Configure input and output devices

--- SimpleCamera/SimpleCameraController.swift
+++ SimpleCamera/SimpleCameraController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SimpleCameraController: UIViewController {
 
     @IBOutlet var cameraButton:UIButton!
+    
+    // Input variables
+    let captureSession = AVCaptureSession()
+    var backFacingCamera: AVCaptureDevice?
+    var frontFacingCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice!
+    
+    // Output variables
+    var stillImageOutput: AVCapturePhotoOutput!
+    var stillImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,33 @@ class SimpleCameraController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func configure() {
+        // Preset the session for taking the photo in full resolution
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        // Get the front and back-facing camera for taking photo
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
+        
+        for device in deviceDiscoverySession.devices {
+            if device.position == .back {
+                backFacingCamera = device
+            } else if device.position == .front {
+                frontFacingCamera = device
+            }
+        }
+        
+        // By default, camera app uses the back-facing camera when it's first enabled
+        currentDevice = backFacingCamera
+        
+        guard let captureDeviceInput = try?
+                AVCaptureDeviceInput(device: currentDevice) else {
+                    return
+                }
+        
+        // Configure the session for outputting still images
+        stillImageOutput = AVCapturePhotoOutput()
     }
     
     // MARK: - Action methods
